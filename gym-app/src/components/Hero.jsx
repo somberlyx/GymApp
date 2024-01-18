4909906cf9d5f25858985ef66efc6c10d6b26fec
somberlyx <lybasiddiqui02@gmail.com>
2024-01-18 20:00:10 +0400

    Add padding to hero component

--- gym-app/src/components/Hero.jsx
+++ gym-app/src/components/Hero.jsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <div className="min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto">
+    <div className="min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4">
       <div className="flex flex-col gap-2">
         <p>IT'S TIME TO GET</p>
         <h1 className="uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
